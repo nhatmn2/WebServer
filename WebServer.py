1db714156b3b977d65bc77efce06771056eb2d4e
nhatmn2 <38064467+nhatmn2@users.noreply.github.com>
2020-09-15 18:05:09 -0700

    skeleton of the structure

--- WebServer.py
+++ WebServer.py
@@ -1,1 +1,61 @@
-print("hello")
+#
+# CPSC-471 Assignment 1
+# Add your name here
+#
+# Add various socket calls in various places, as required.
+# Just a hint, see the sample TCPServer.py in the book
+#
+# Disclaimer:  There are no guarantees that the comments or
+#              the code in this file are very accurate
+#
+
+#import socket module
+from socket import *
+import sys                       # In order to terminate the program
+
+# Create a TCP server socket
+# AF_INET is used for IPv4 protocols
+# SOCK_STREAM is used for TCP
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+#Prepare a server socket
+#Fill in start
+#Fill in end
+
+while True:
+    #Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr =     #Fill in start #Fill in end
+
+    try:
+        message =  #Fill in start #Fill in end
+
+        filename = message.split()[1]
+
+        f = open(filename[1:])
+
+        outputdata = #Fill in start #Fill in end
+
+        #Send one HTTP header line into socket
+        #Fill in start
+        #Fill in end
+
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+
+    except IOError:
+        #Send response message for file not found
+        #Fill in start
+        #Fill in end
+
+        #Close client socket
+        #Fill in start
+        #Fill in end
+
+serverSocket.close()
+
+sys.exit()        #Terminate the program after sending the corresponding data
